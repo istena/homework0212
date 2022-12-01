96063293d8a97771dafb50a071b23f29c0f250f0
Ilya <sannikov444@yandex.ru>
2022-12-01 18:22:01 +0300

    homework

---
+++ taskdp/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите количество чисел: ");
+int a= Convert.ToInt32(Console.ReadLine());
+int []array=new int [a];
+Console.Write("Введите чисела: ");
+for(int i=0;i<a;i++){
+    array[i]=Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Введите сдвиг: ");
+int s=Convert.ToInt32(Console.ReadLine());
+int []newarray=new int [a];
+int q=0;
+for(int i=0;i<a;i++){
+    q=i+s+1;
+    if(q>=a) {
+        q=q%a; 
+    }
+    newarray[i]=array[q];
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", newarray)}]");
