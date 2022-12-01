3fd935e8acc96142fd163197f840faed1f87aa60
Ilya <sannikov444@yandex.ru>
2022-12-01 11:54:49 +0300

    25

---
+++ task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b= Convert.ToInt32(Console.ReadLine());
+int res=1;
+for (int i=0;i<b;i++){
+    res=res*a;
+}
+Console.Write($"{a} в степени {b} равно {res} ");
