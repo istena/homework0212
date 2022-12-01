20dda611ff31e9b0d69169fd7626ba61e2998204
Ilya <sannikov444@yandex.ru>
2022-12-01 11:54:59 +0300

    27

---
+++ task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a= Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while ((a/10)>0){
+    sum+=(a%10);
+    a=a/10;
+}
+sum+=(a%10);
+Console.Write($"Сумма чисел = {sum}  ");
